65cd7fd62a1aa48866804b161125c9ce5d93e1f5
James Young <46870524+jamesyoung93@users.noreply.github.com>
2025-05-17 13:29:43 -0400

    Update 02_build_blast_db.sh

--- scripts/02_build_blast_db.sh
+++ scripts/02_build_blast_db.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
+
 mkdir -p data/db
 cat data/raw/*/*.faa > data/db/all_cyano.faa
-makeblastdb -in data/db/all_cyano.faa -dbtype prot -out data/db/cyano_db
+
+echo "[MAKEBLASTDB] building protein DB…"
+makeblastdb \
+  -in data/db/all_cyano.faa \
+  -dbtype prot \
+  -title cyano_db \
+  -out data/db/cyano_db
+
+echo "[DONE] BLAST DB in data/db/cyano_db.*"
